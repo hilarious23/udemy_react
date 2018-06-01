259bf3a1c361487f0304fe73920867d34f509da5
hilarious23 <s.positex11@gmail.com>
2018-06-01 17:26:11 +0900

    State and Props, change input

---
+++ src/components/app.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import Greeting from './greeting';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'John',
+    };
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.handleNameChange(e)}
+        />
+        <Greeting name={this.state.name} />
+      </div>
+    );
+  }
+}
+
+export default App;
